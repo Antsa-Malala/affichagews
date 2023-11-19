6b17fe164057a110eab497f4352554e9c584835d
Antsa-Malala <malalanirina030304@gmail.com>
2023-11-19 17:28:48 +0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import FormLogin from './components/Body/FormLogin';
+
+function App() {
+  return (
+    <div>
+      <FormLogin></FormLogin>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body/FormLogin.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./FormLogin.css";
+function FormLogin() {
+    return (
+        <div>
+           <form action="http://localhost:8080/apiToken/login" method="post">
+                <div>
+                    <label>
+                        Username
+                        <input type="text" name="username" id="username"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password
+                        <input type="password" name="password" id="password"/>
+                    </label>
+                </div>
+                <input type="submit" value="Login" />
+           </form>
+        </div>
+    );
+}
+
+export default FormLogin
